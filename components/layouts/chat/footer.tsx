05947568a52850278d51ec6442903a5dff560e78
tontan2545 <tontan2545@gmail.com>
2023-10-13 23:54:13 +0700

    fix: adjust padding and submit on enter

--- components/layouts/chat/footer.tsx
+++ components/layouts/chat/footer.tsx
@@ -1,24 +1,39 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
 import { Send } from "lucide-react";
-import React from "react";
+import React, { useState } from "react";
 
 type Props = {};
 
 const Footer = (props: Props) => {
+  const [text, setText] = useState<string>();
   return (
     <div className="flex flex-col flex-grow-0 flex-shrink-0 h-32 justify-end items-center space-y-2 pb-8">
       <div
         className="flex space-x-2 items-end w-3/5
       "
       >
-        <Textarea
-          className="resize-none h-full"
-          minRows={1}
-          maxRows={4}
-          placeholder="Send a message here"
-        />
-        <Button className="pl-[10px] pr-3">
+        <div className="flex items-center justify-center h-full w-full bg-input rounded-md placeholder:text-gray-300">
+          <Textarea
+            className="resize-none px-5"
+            minRows={1}
+            maxRows={4}
+            placeholder="Send a message here"
+            value={text}
+            onKeyDown={(e) => {
+              if (e.key === "Enter" && !e.shiftKey) {
+                e.preventDefault();
+                console.log("send message");
+              }
+            }}
+            onChange={(e) => setText(e.target.value ?? "")}
+          />
+        </div>
+        <Button
+          className="pl-[10px] pr-3 transition-all duration-150"
+          disabled={!text || text.length === 0}
+        >
           <Send />
         </Button>
       </div>
