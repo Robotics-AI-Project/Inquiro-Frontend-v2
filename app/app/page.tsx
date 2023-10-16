e80dffe3517de086d9caf88f54d907faec4a5ff0
tontan2545 <tontan2545@gmail.com>
2023-10-16 13:47:21 +0700

    fix: remove 2 chat in home

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,10 +1,3 @@
-import Message from "@/components/layouts/chat/message";
-
 export default function Home() {
-  return (
-    <div>
-      <Message sender="USER" message="Hello" />
-      <Message sender="AGENT" message="Hi" />
-    </div>
-  );
+  return <div></div>;
 }
